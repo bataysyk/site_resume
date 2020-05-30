fca9dd6dfb989f792c9a593e7328068efc65f274
Your Name <you@example.com>
2020-05-30 22:00:41 +0300

    init

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.admin import User
+# from django.contrib.auth.admin import User
+from django.contrib.auth.models import User
 from django.http import JsonResponse
 from .models import UserFrom
 
@@ -10,10 +11,13 @@ def sign_in(request):
     if request.method == "GET":
         return render(request, "account/sign_in.html")
     if request.method == "POST":
-        username = request.POST.get("inputUsername")
-        password = request.POST.get("inputPassword")
-        if username and password:
-            user = authenticate(username=username, password=password)
+        username_q = request.POST.get("inputUsername")
+        password_q = request.POST.get("inputPassword")
+        if username_q and password_q:
+            print(username_q)
+            print(password_q)
+            user = authenticate(username=username_q, password=password_q)
+            print(user)
             if user is not None:
                 login(request, user)
                 print(user)
@@ -60,23 +64,23 @@ def ajax_password_to_test(request):
 def sign_up(request):
     data = dict()
     form = UserFrom(request.POST or None, request.FILES or None)
+    data["form"] = form
     if request.method == "GET":
-        print("OK")
-        data["form"] = form
         return render(request, "account/sign_up.html", context=data)
     elif request.method == "POST":
-        print("2")
         username = request.POST.get("username")
         email = request.POST.get("email")
         password_1 = request.POST.get("password_1")
         password_2 = request.POST.get("password_2")
         if username and email and password_1 and password_2:
-            print("1")
+            print(password_1)
+            print(password_2)
             if password_1 == password_2:
-                user = User(
-                    username=username,
-                    email=email,
-                    password=password_1
-                )
+                user = User.objects.create_user(username=username, email=email, password=password_1)
+                # user = User(
+                #     username=username,
+                #     email=email,
+                #     password=password_1,
+                # )
                 user.save()
         return redirect("/home/")
